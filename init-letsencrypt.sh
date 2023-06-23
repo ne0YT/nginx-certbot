ec0c27e4184279727175f13c772b3cf1dcc41595
ne0YT <97364807+ne0YT@users.noreply.github.com>
2023-06-23 18:45:03 +0200

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,7 +3,7 @@
 AUTO_YES=false
 NO_WILDCARD=false
 
-domains=(DOMAIN_BASE *.DOMAIN_BASE)
+domains=("DOMAIN_BASE" "*.DOMAIN_BASE")
 rsa_key_size=4096
 data_path="./data/certbot"
 email="info@DOMAIN_BASE" # Adding a valid address is strongly recommended
@@ -19,7 +19,7 @@ do
     ;;
     --no-wildcard)
     NO_WILDCARD=true
-    domains=MAINSUBDOMAIN.DOMAIN_BASE # Only the Subdomain, not the wildcard
+    domains=("MAINSUBDOMAIN.DOMAIN_BASE") # Only the Subdomain, not the wildcard
     shift # Remove --no-wildcard from processing
     ;;
     *)
@@ -29,20 +29,22 @@ do
   esac
 done
 
+# Join domains with space for usage in commands
+domains_string="${domains[*]}"
+
 # Replace placeholders with user-provided values
-grep -rl 'SUBDOMAIN_or_DOMAINBASE' . --exclude-dir=.git | xargs sed -i "s/SUBDOMAIN_or_DOMAINBASE/$domains/g"
+grep -rl 'SUBDOMAIN_or_DOMAINBASE' . --exclude-dir=.git | xargs sed -i "s/SUBDOMAIN_or_DOMAINBASE/$domains_string/g"
 
 if [ -d "$data_path" ]; then
   if [[ $AUTO_YES == true ]]; then
     echo "Automatically responding 'yes' to replace."
   else
-    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+    read -p "Existing data found for $domains_string. Continue and replace existing certificate? (y/N) " decision
     if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
       exit
     fi
   fi
 fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -52,9 +54,9 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+echo "### Creating dummy certificate for $domains_string ..."
+path="/etc/letsencrypt/live/$domains_string"
+mkdir -p "$data_path/conf/live/$domains_string"
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -62,29 +64,23 @@ docker compose run --rm --entrypoint "\
     -subj '/CN=localhost'" certbot
 echo
 
-
 echo "### Starting nginx ..."
 docker compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+echo "### Deleting dummy certificate for $domains_string ..."
 docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$domains_string && \
+  rm -Rf /etc/letsencrypt/archive/$domains_string && \
+  rm -Rf /etc/letsencrypt/renewal/$domains_string.conf" certbot
 echo
 
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
+echo "### Requesting Let's Encrypt certificate for $domains_string ..."
 #Join $domains to -d args
 domain_args=""
-if [ "$NO_WILDCARD" = true ]; then
-  domain_args="-d $domains"
-else
-  for domain in "${domains[@]}"; do
-    domain_args="$domain_args -d $domain"
-  done
-fi
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
 
 # Select appropriate email arg
 case "$email" in
@@ -96,17 +92,17 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker compose run --rm --entrypoint "\
-        certbot certonly \
-                $staging_arg \
-                $email_arg \
-                -d $domain_args \
-                --rsa-key-size $rsa_key_size \
-                --no-eff-email \
-                --agree-tos \
-                --force-renewal \
-                --dns-cloudflare \
-                --dns-cloudflare-credentials /etc/letsencrypt/cf.ini \
-                --dns-cloudflare-propagation-seconds 30" certbot
+  certbot certonly \
+          $staging_arg \
+          $email_arg \
+          $domain_args \
+          --rsa-key-size $rsa_key_size \
+          --no-eff-email \
+          --agree-tos \
+          --force-renewal \
+          --dns-cloudflare \
+          --dns-cloudflare-credentials /etc/letsencrypt/cf.ini \
+          --dns-cloudflare-propagation-seconds 30" certbot
 echo
 
 echo "### Reloading nginx ..."
